f3e5b467dd979dad67df257dbe289d08ebd34e96
psguazz <p.sguazz@gmail.com>
2024-03-13 11:20:40 +0100

    Explicit module definition
    Nesting modules lets us get rid of the extra files.

--- lib/dato_cms_graphql/rails.rb
+++
@@ -1,2 +0,0 @@
-module DatoCmsGraphql::Rails
-end
--- lib/dato_cms_graphql/rails/persistence.rb
+++ lib/dato_cms_graphql/rails/persistence.rb
@@ -1,27 +1,29 @@
-module DatoCmsGraphql::Rails::Persistence
-  def self.persist_record(query, record)
-    Object.const_get(query.query_name)
-      .find_or_create_by(
-        locale: I18n.locale,
-        cms_id: record.id
-      )
-      .update(
-        render: query.render?,
-        permalink: (record.permalink if record.respond_to?(:permalink)),
-        cms_record: record.localized_raw_attributes
-      )
-  end
+module DatoCmsGraphql::Rails
+  module Persistence
+    def self.persist_record(query, record)
+      Object.const_get(query.query_name)
+        .find_or_create_by(
+          locale: I18n.locale,
+          cms_id: record.id
+        )
+        .update(
+          render: query.render?,
+          permalink: (record.permalink if record.respond_to?(:permalink)),
+          cms_record: record.localized_raw_attributes
+        )
+    end
 
-  def self.cache_data
-    DatoCmsGraphql.queries.each do |query|
-      I18n.available_locales.each do |locale|
-        I18n.with_locale(locale) do
-          if query.single_instance?
-            record = query.get
-            persist_record(query, record)
-          else
-            query.all.each do |record|
+    def self.cache_data
+      DatoCmsGraphql.queries.each do |query|
+        I18n.available_locales.each do |locale|
+          I18n.with_locale(locale) do
+            if query.single_instance?
+              record = query.get
               persist_record(query, record)
+            else
+              query.all.each do |record|
+                persist_record(query, record)
+              end
             end
           end
         end
