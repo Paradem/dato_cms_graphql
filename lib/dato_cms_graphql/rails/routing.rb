820cef9b58a3e762bb2402f6697ebb22d5bb8bab
Kevin Pratt <kevin@paradem.co>
2024-04-18 16:04:37 -0600

    Move the root definition to the end of the list

--- lib/dato_cms_graphql/rails/routing.rb
+++ lib/dato_cms_graphql/rails/routing.rb
@@ -4,19 +4,17 @@ module DatoCmsGraphql::Rails
       ::Rails.application.routes.draw do
         scope "(:locale)", locale: I18n.default_locale do
           # Queries that represent collections of pages.
-          DatoCmsGraphql.renderable.each do |query|
+          DatoCmsGraphql.renderable.select { |q| !q.route.blank? }.each do |query|
             controller = query.plural_name.underscore
-
-            if query.route.blank?
-              next
-            else
-              get(
-                query.route,
-                to: "#{controller}#show",
-                constraints: lambda { |request| base_class.allow?(query.query_name, request) }
-              )
-            end
+            get(
+              query.route,
+              to: "#{controller}#show",
+              constraints: lambda { |request| base_class.allow?(query.query_name, request) }
+            )
           end
+
+          home = DatoCmsGraphql.renderable.find { |q| q.route.blank? }
+          root "#{home.plural_name.underscore}#show"
         end
       end
     end
